a2c238595281959c6b9a63a50c0b15f71de447fa
Parbat Lama <lamaparbat70@gmail.com>
2024-05-14 22:35:18 +0545

    Feat: push latest scrap tag photos

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import initInstaTagsScrapCron from "./src/modules/insta/utils/cron";
 initConnections().then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log("Server listening on port " + process.env.PORT);
-        initInstaTagsScrapCron();
+        // initInstaTagsScrapCron();
     });
 });
 
--- src/modules/media/services/index.ts
+++ src/modules/media/services/index.ts
@@ -1,8 +1,9 @@
 import fs, { access } from "fs";
 
+console.log( __dirname?.split("INSTA-SCRAPING")?.[0] + "INSTA-SCRAPING/assets/uploads/")
 const getMediaByPath = async ({ filepath }: { filepath: string }) => {
     try {
-        const fullPath = __dirname?.split("src")?.[0] + "assets/uploads/" + filepath;
+        const fullPath = __dirname?.split("INSTA-SCRAPING")?.[0] + "INSTA-SCRAPING/assets/uploads/" + filepath;
 
         return await new Promise((resolve, reject) => {
             access(fullPath, fs.constants.F_OK, (err) => {
